3e2404e2eb2df6ef75ba50db2a8b8e37deb76fa5
Sai Chaitanya Reddy <saichaithanya1999@gmail.com>
2021-02-20 09:35:07 +0530

    initial commit

---
+++ src/main/kotlin/com/axis/jgbbackend/JourneyGraphBoardBackendApplication.kt
@@ -0,0 +1,11 @@
+package com.axis.jgbbackend
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class JourneyGraphBoardBackendApplication
+
+fun main(args: Array<String>) {
+	runApplication<JourneyGraphBoardBackendApplication>(*args)
+}
